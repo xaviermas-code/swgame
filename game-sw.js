e9286e7e8d156ff8e613f3c58971833fb4061bfa
Xavier Mas <xaviermas.code@gmail.com>
2025-06-08 12:52:36 +0200

    Moving ship

--- game-sw.js
+++ game-sw.js
@@ -30,22 +30,135 @@ const Ship = {
         y:0
     },
 
+    rotation:0,
+
 draw() {
-    c.drawImage(this.src, this.position.x, this.position.y, this.sizex, this.sizey )
+    c.save()
+    c.translate(this.position.x + this.sizex / 2, this.position.y + this.sizey / 2)
+     c.rotate(this.rotation);
+    c.drawImage(this.src, -this.sizex / 2,
+        -this.sizey / 2, this.sizex, this.sizey )
+    c.restore();
 
 },
 
+update() {
+if(this.src.complete) {
+    this.draw()
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+}
+    },
 
+};
+shipImage.onload = () => {
+        Ship.draw();
+        animate();
+}
+
+const keys = {
+        a: {
+            pressed: false
+        },
+        d: {
+            pressed: false
+        },
+        space: {
+            pressed: false
+        },
+        w: {
+            pressed: false
+        },
+        s: {
+            pressed: false
+        }
     };
 
 
-shipImage.onload = () => {
-        Ship.draw();
+addEventListener("keyup", ({key}) => {
+    switch (key) {
+        case "a":
+            keys.a.pressed = false;
+            break;
+        case "d":
+            keys.d.pressed = false;
+            break;
+        case "w":
+            keys.w.pressed = false;
+            break;
+        case "s":
+            keys.s.pressed = false;
+            break;
+    }
+});
 
 
+addEventListener("keydown",({key}) =>{
+        switch (key) {
+            case "a": 
+            console.log("left")
+            keys.a.pressed = true
+            break
+            case "d":
+            console.log("right")
+            keys.d.pressed = true
+            break
+            case " ":
+            console.log("space")
+            break
+            case "w":
+            keys.w.pressed = true
+            break
+            case "s":
+            keys.s.pressed = true
+    
+        }
+    })
+
+
+
+
+
+
+
+
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.fillStyle = "black";
+    c.fillRect(0,0, canvas.width, canvas.height)
+    
+    Ship.update()
+
+// Movimiento horizontal
+if (keys.a.pressed && Ship.position.x >=0) {
+    Ship.velocity.x = -5;
+    Ship.rotation = -0.15;
+} else if (keys.d.pressed && Ship.position.x + Ship.sizex <= innerWidth) {
+    Ship.velocity.x = 5;
+    Ship.rotation = 0.15;
+} else {
+    Ship.velocity.x = 0;
+    Ship.rotation = 0;
 }
 
+// Movimiento vertical
+if (keys.w.pressed && Ship.position.y >=0) {
+    Ship.velocity.y = -5;
+} else if (keys.s.pressed && Ship.position.y + Ship.sizey <= innerHeight) {
+    Ship.velocity.y = 5;
+} else {
+    Ship.velocity.y = 0;
+}
+
+
 };
+
+
+};
+
+
+
 
 
 
